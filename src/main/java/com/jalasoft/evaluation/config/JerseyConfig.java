618ea1e8ad4b8892297f3566c5c036f3b4215f3e
Richard Gutierrez Condori <Richard.Gutierrez@jalasoft.com>
2021-10-13 15:00:46 -0500

    ET-001: Update configuration to build

--- src/main/java/com/jalasoft/evaluation/config/JerseyConfig.java
+++ src/main/java/com/jalasoft/evaluation/config/JerseyConfig.java
@@ -1,14 +1,14 @@
 package com.jalasoft.evaluation.config;
 
-import com.jalasoft.evaluation.evaluationtest.controllers.EvaluationController;
-import com.jalasoft.evaluation.home.controller.HomeController;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.ApplicationPath;
+
 @Component
+@ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
-        register(HomeController.class);
-        register(EvaluationController.class);
+        packages("com.jalasoft.evaluation");
     }
 }
